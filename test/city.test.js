d0bfe38dd105c83eb88c03647d01b84f1883a62c
JsonMa <mahao@iot.chinamobile.com>
2017-09-05 08:52:27 +0800

    修改API名称

--- test/city.test.js
+++ test/city.test.js
@@ -18,32 +18,32 @@ describe('test api through input type', () => {
 
   it('should return city info through code', async () => {
     const ctx = app.mockContext();
-    const cityInfo = await ctx.service.City.getCity('360000');
+    const cityInfo = await ctx.service.city.getCity('360000');
     assert(cityInfo[0].code === '360000');
   });
 
   it('should return city info through name', async () => {
     const ctx = app.mockContext();
-    const cityInfo = await ctx.service.City.getCity('宿迁市');
+    const cityInfo = await ctx.service.city.getCity('宿迁市');
     assert(cityInfo[0].name === '宿迁市');
   });
 
   describe('test api through city type', () => {
     it('should return province info', async () => {
       const ctx = app.mockContext();
-      const province = await ctx.service.City.getCity('320000');
+      const province = await ctx.service.city.getCity('320000');
       assert(province[0].code === '320000');
     });
 
     it('should return city info', async () => {
       const ctx = app.mockContext();
-      const city = await ctx.service.City.getCity('139000');
+      const city = await ctx.service.city.getCity('139000');
       assert(city[0].code === '139000');
     });
 
     it('should return distinct info', async () => {
       const ctx = app.mockContext();
-      const distinct = await ctx.service.City.getCity('410922');
+      const distinct = await ctx.service.city.getCity('410922');
       assert(distinct[0].code === '410922');
     });
   });
